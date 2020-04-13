fa87b985f6ae88e057893b386825047d9c27f222
@Marcin1136 <marcinwypior1990@gmail.com>
2020-04-13 16:45:08 +0200

    symulator Lotto na listach jest duzo latwiejszy

--- src/pl/coderslab/gra2/Lotto.java
+++ src/pl/coderslab/gra2/Lotto.java
@@ -1,113 +1,177 @@
 package pl.coderslab.gra2;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Lotto {
     public static void main(String[] args) {
 
-        int []randomNumbers=getRandomNumbers();
-        System.out.println("Random numbers" + Arrays.toString(randomNumbers));
+        Integer[]allNumbers=new Integer[49];
 
-        int []yourNumbers=getYourNumbers();
-        System.out.println("Your numbers:" + Arrays.toString(yourNumbers));
-
-        if(compareNumbers(randomNumbers, yourNumbers)>3){
-            System.out.println("You matched "+compareNumbers(randomNumbers, yourNumbers)+" numbers correctly !!!\n CONGRATULATIONS !!!");
-        }else{
-            System.out.println("you should get a descent job , lotteries are for lazy fools");
+        for (int i = 0; i <=allNumbers.length -1; i++) {
+            allNumbers[i]=i+1;
         }
 
-
-    }
-
-    static int[] getRandomNumbers(){
-
-        Random r=new Random();
-        Scanner scan= new Scanner(System.in);
-        int []luckyNumbers=new int[6];
-        int []yourTicket=new int[6];
-
-        for (int i = 0; i <= luckyNumbers.length-1; i++) {
-            luckyNumbers[i]=r.nextInt(48)+1;
-
-            for (int j = 0; j < i-1; j++) {
-
-                if(luckyNumbers[i]==luckyNumbers[j]){
-
-
-                    //System.out.println(luckyNumbers[i]+" "+luckyNumbers[i]);
-                    //System.out.println("powtorzyly sie");
-                    i--;
-                }
-
-            }
-
-            //System.out.println(luckyNumbers[i]);
-        }
-
+        Collections.shuffle(Arrays.asList(allNumbers));
+        Integer [] luckyNumbers=Arrays.copyOf(allNumbers,6);
         Arrays.sort(luckyNumbers);
-        return luckyNumbers;
-    }
+        System.out.println("winning numbers are:" + Arrays.toString(luckyNumbers));
+        List <Integer> myNumbers=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
 
-    static int [] getYourNumbers(){
+        System.out.println("which numbers do you bet ? \nenter 6 numbers");
 
-        int []yourNumbers=new int[6];
-        Scanner scan= new Scanner(System.in);
+        for (int i = 0; i <6 ; i++) {
 
-        for (int i = 0; i <=yourNumbers.length -1 ; i++) {
-
-            System.out.print("enter number "+(i+1)+":");
-            while (!scan.hasNextInt()){
-                scan.nextLine();
-                System.out.println("must be a number, enter "+(i+1)+ " again:");
+            Integer buffer;
+            System.out.println("enter number "+(i+1));
+            while (!sc.hasNextInt()) {
+                System.out.println("zly format");
+                sc.nextLine();
             }
+            buffer=sc.nextInt();
 
-
-            yourNumbers[i] = scan.nextInt();
-
-            for (int j = 0; j <i ; j++) {
-                if(yourNumbers[i]==yourNumbers[j]){
-                    System.out.println("your ticket aleready contains this number");
-                    i--;
-                    break;
-                }
-            }
-
-            if((yourNumbers[i]<1)||(yourNumbers[i]>49)) {
+            if((buffer>49)||(buffer<1)){
+                System.out.println("number out of range 1 - 49");
                 i--;
-                System.out.println("out of range 1-49");
             }
 
 
+            if(!myNumbers.contains(buffer)) {
+                myNumbers.add(buffer);
+            }else {
+                i--;
+                System.out.println("this number is aleredy on your ticket");
+            }
         }
 
-        //System.out.println(Arrays.toString(yourNumbers));
-        Arrays.sort(yourNumbers);
-        return yourNumbers;
-    }
+        Collections.sort(myNumbers);
+        System.out.println(myNumbers.toString());
 
-    static int compareNumbers(int [] a,int []b){
 
         int counter=0;
+        for (Integer n:luckyNumbers
+        ) {
 
-        for (int i = 0; i <= a.length - 1; i++) {
-
-            for (int j = 0; j <= b.length -1  ; j++) {
-
-                if(a[i]==b[j]){
-                    counter++;
-
-                    //System.out.println("trafiles liczbe "+a[i]);
-
-                }
+            if(myNumbers.contains(n)){
+                counter++;
+                System.out.println("you guessed number: "+ n);
             }
-
-
         }
 
-
-        return counter;
+        if(counter>=3)
+            System.out.println("you guesst "+counter+" numbers");
     }
 }
+
+//wersja na tablicach bez list
+//import java.util.Arrays;
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Lotto {
+//    public static void main(String[] args) {
+//
+//        int []randomNumbers=getRandomNumbers();
+//        System.out.println("Random numbers" + Arrays.toString(randomNumbers));
+//
+//        int []yourNumbers=getYourNumbers();
+//        System.out.println("Your numbers:" + Arrays.toString(yourNumbers));
+//
+//        if(compareNumbers(randomNumbers, yourNumbers)>3){
+//            System.out.println("You matched "+compareNumbers(randomNumbers, yourNumbers)+" numbers correctly !!!\n CONGRATULATIONS !!!");
+//        }else{
+//            System.out.println("you should get a descent job , lotteries are for lazy fools");
+//        }
+//
+//
+//    }
+//
+//    static int[] getRandomNumbers(){
+//
+//        Random r=new Random();
+//        Scanner scan= new Scanner(System.in);
+//        int []luckyNumbers=new int[6];
+//        int []yourTicket=new int[6];
+//
+//        for (int i = 0; i <= luckyNumbers.length-1; i++) {
+//            luckyNumbers[i]=r.nextInt(48)+1;
+//
+//            for (int j = 0; j < i-1; j++) {
+//
+//                if(luckyNumbers[i]==luckyNumbers[j]){
+//
+//
+//                    //System.out.println(luckyNumbers[i]+" "+luckyNumbers[i]);
+//                    //System.out.println("powtorzyly sie");
+//                    i--;
+//                }
+//
+//            }
+//
+//            //System.out.println(luckyNumbers[i]);
+//        }
+//
+//        Arrays.sort(luckyNumbers);
+//        return luckyNumbers;
+//    }
+//
+//    static int [] getYourNumbers(){
+//
+//        int []yourNumbers=new int[6];
+//        Scanner scan= new Scanner(System.in);
+//
+//        for (int i = 0; i <=yourNumbers.length -1 ; i++) {
+//
+//            System.out.print("enter number "+(i+1)+":");
+//            while (!scan.hasNextInt()){
+//                scan.nextLine();
+//                System.out.println("must be a number, enter "+(i+1)+ " again:");
+//            }
+//            yourNumbers[i] = scan.nextInt();
+//
+//
+//
+//            for (int j = 0; j <i ; j++) {
+//                if(yourNumbers[i]==yourNumbers[j]){
+//                    System.out.println("your ticket aleready contains this number");
+//                    i--;
+//                    break;
+//                }
+//            }
+//
+//            if((yourNumbers[i]<1)||(yourNumbers[i]>49)) {
+//                i--;
+//                System.out.println("out of range 1-49");
+//            }
+//
+//
+//        }
+//
+//        //System.out.println(Arrays.toString(yourNumbers));
+//        Arrays.sort(yourNumbers);
+//        return yourNumbers;
+//    }
+//
+//    static int compareNumbers(int [] a,int []b){
+//
+//        int counter=0;
+//
+//        for (int i = 0; i <= a.length - 1; i++) {
+//
+//            for (int j = 0; j <= b.length -1  ; j++) {
+//
+//                if(a[i]==b[j]){
+//                    counter++;
+//
+//                    //System.out.println("trafiles liczbe "+a[i]);
+//
+//                }
+//            }
+//
+//
+//        }
+//
+//
+//        return counter;
+//    }
+//}
